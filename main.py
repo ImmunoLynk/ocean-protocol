ca24285274398b1d8bf1ffa10b86f4b8d562e742
Ruan Comelli <ruancomelli@gmail.com>
2020-06-06 13:29:55 -0300

    transaction function updated

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import flask
 from flask_cors import CORS
 
-from src.account import _get_account
+from web3 import Web3
 
 from ocean_utils.agreements.service_types import ServiceTypes
 from mantaray_utilities.events import subscribe_event
-from ocean_keeper.web3_provider import Web3Provider
 from ocean_keeper import Keeper
 from squid_py import (
     Ocean,
@@ -13,6 +12,7 @@ from squid_py import (
 )
 
 from src import default_config_dict, metadata
+from src.account import get_account
 
 FLASK_APPLICATION_NAME = 'ocean-protocol'
 TRANSACTION_RULE = '/transaction'
@@ -28,7 +28,7 @@ CORS(app)
 ocn = Ocean(configuration)
 keeper = Keeper.get_instance()
 
-MARKET_PLACE_PROVIDER_ADDRESS = Web3Provider.get_web3().toChecksumAddress(configuration.provider_address)
+MARKET_PLACE_PROVIDER_ADDRESS = Web3.toChecksumAddress(configuration.provider_address)
 
 
 @app.route(TRANSACTION_RULE, methods=['POST'])
@@ -50,49 +50,50 @@ def process_transaction():
     """
 
     # initialize return dict
-    data = {"success": False}
+    data = {'success': False}
 
-    if flask.request.method == "POST":
+    if flask.request.method == 'POST':
 
         # get file containing all information needed for transaction happen
         _file = flask.request.files.get('transaction')
         if _file:
-            publisher_acct = _get_account(_file.get("publisher"))
-            consumer_acct = _get_account(_file.get("consumer"))
+            publisher_acct = get_account(_file.get('publisher'))
+            consumer_acct = get_account(_file.get('consumer'))
 
             # update the price
             metadata.update({
-                "price": float(_file.get("price"))
+                'price': float(_file.get('price'))
             })
 
             ddo = ocn.assets.create(metadata, publisher_acct, providers=[MARKET_PLACE_PROVIDER_ADDRESS])
+            service_index = ddo.get_service(ServiceTypes.ASSET_ACCESS).index
+            
             agreement_id = ocn.assets.order(
                 ddo.did,
-                ddo.get_service(ServiceTypes.ASSET_ACCESS).index,
+                service_index,
                 consumer_acct,
                 provider_address=MARKET_PLACE_PROVIDER_ADDRESS
             )
 
-            subscribe_event("created agreement", keeper, agreement_id)
-            subscribe_event("lock reward", keeper, agreement_id)
-            subscribe_event("access secret store", keeper, agreement_id)
-            subscribe_event("escrow reward", keeper, agreement_id)
+            subscribe_event('created agreement', keeper, agreement_id)
+            subscribe_event('lock reward', keeper, agreement_id)
+            subscribe_event('access secret store', keeper, agreement_id)
+            subscribe_event('escrow reward', keeper, agreement_id)
 
             ocn.assets.consume(
                 agreement_id,
                 ddo.did,
-                'Access',
+                service_index,
                 consumer_acct,
-                'downloads_nile'
+                configuration.downloads_path
             )
 
             # update request status
-
             data.update({
-                "success": True
+                'success': True
             })
 
     return flask.jsonify(data)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
